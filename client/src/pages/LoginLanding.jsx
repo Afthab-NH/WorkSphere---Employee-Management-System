ad8b540cf6b7fe364f02af642ebfbda036301b7e
Afthab NH <afthabmohammed45@gmail.com>
2026-05-05 23:20:13 +0530

    landing pages created

---
+++ client/src/pages/LoginLanding.jsx
@@ -0,0 +1,10 @@
+
+const loginLanding = () => {
+  return (
+    <div>
+      LoginLanding
+    </div>
+  )
+}
+
+export default loginLanding
---
+++ client/src/pages/PrintPayslip.jsx
@@ -0,0 +1,10 @@
+
+const PrintPayslip = () => {
+  return (
+    <div>
+      print payslips
+    </div>
+  )
+}
+
+export default PrintPayslip
