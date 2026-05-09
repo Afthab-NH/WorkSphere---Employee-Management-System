44a9a7f5951ceca46ab3def347a3e6ace07fac69
Afthab NH <afthabmohammed45@gmail.com>
2026-05-09 23:16:18 +0530

    right side panel setup

--- client/src/pages/LoginLanding.jsx
+++ client/src/pages/LoginLanding.jsx
@@ -8,10 +8,19 @@ const LoginLanding = () => {
       <div className="w-full md:w-1/2 flex flex-col items-center justify-center p-6 sm:p-12 lg:p-16 relative overflow-y-auto min-h-screen">
       <div className="w-full max-w-md animate-fade-in relative z-10">
         {/* header */}
+        <div className="mb-10 text-center md:text-left">
+          <h2 className="text-3xl font-medium text-slate-900 tracking-tight mb-3">Welcome Back!</h2>
+          <p>
+            Select your portal to securely access the system.
+          </p>
+        </div>
 
         {/* Portals list */}
 
+
         {/* footer */}
+
+
       </div>
 
       </div>
