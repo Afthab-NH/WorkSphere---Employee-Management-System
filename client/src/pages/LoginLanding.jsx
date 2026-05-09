dab8e5da1a5700c45c1c4888db6b6812e09ad5fc
Afthab NH <afthabmohammed45@gmail.com>
2026-05-09 23:11:25 +0530

    left side setup

--- client/src/pages/LoginLanding.jsx
+++ client/src/pages/LoginLanding.jsx
@@ -4,8 +4,21 @@ const LoginLanding = () => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <LoginLeftside />
+
+      <div className="w-full md:w-1/2 flex flex-col items-center justify-center p-6 sm:p-12 lg:p-16 relative overflow-y-auto min-h-screen">
+      <div className="w-full max-w-md animate-fade-in relative z-10">
+        {/* header */}
+
+        {/* Portals list */}
+
+        {/* footer */}
+      </div>
+
+      </div>
     </div>
   )
 }
 
 export default LoginLanding
+
+//41:49
