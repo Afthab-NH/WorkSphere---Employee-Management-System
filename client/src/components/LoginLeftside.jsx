8a9ddbd7040c43200399efaadef4447d81fce0dc
Afthab NH <afthabmohammed45@gmail.com>
2026-05-10 23:57:49 +0530

    ||

--- client/src/components/LoginLeftside.jsx
+++ client/src/components/LoginLeftside.jsx
@@ -8,7 +8,7 @@ const LoginLeftside = () => {
       <div className="relative z-10 flex flex-col items-start justify-center p-12 lg:p-20 w-full h-full">
         <h1 className="text-4xl lg:text-5xl font-medium text-white mb-6 leading-tight tracking-tight"> 
             WorkSphere <br />
-             <h2 className="text-2xl ">Employee Management System </h2>
+            <p className="text-slate-400 text-2xl">Employee Management System</p>
         </h1>
         <p className="text-slate-400 text-lg max-w-md leading-relaxed">
             Streamline your Workforce Operations, Track Attendence, Manage Payroll, and Empower your team securely!
