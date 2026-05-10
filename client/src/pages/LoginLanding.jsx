5d5ce84cc26eee1f274b52cc6dc48d5526575f29
Afthab NH <afthabmohammed45@gmail.com>
2026-05-11 00:16:52 +0530

    login page buttons and footer setup

--- client/src/pages/LoginLanding.jsx
+++ client/src/pages/LoginLanding.jsx
@@ -1,5 +1,5 @@
 import LoginLeftside from "../components/LoginLeftside"
-import { ShieldIcon } from "lucide-react"
+import { ArrowRightIcon, Link, ShieldIcon, UserIcon } from "lucide-react"
 
 const LoginLanding = () => {
   
@@ -13,7 +13,8 @@ const LoginLanding = () => {
     {
       to: "/login/employee",
       title: "Employee Portal",
-      description: "View your profiles,track attendence, request time off, and access payslips."
+      description: "View your profiles,track attendence, request time off, and access payslips.",
+      icon: UserIcon
     }
   ]
 
@@ -33,9 +34,23 @@ const LoginLanding = () => {
         </div>
 
         {/* Portals list */}
+        <div className="space-y-4">
+          {portalOptions.map((portal) => (
+            <Link key={portal.to} to={portal.to} className="group block bg-slate-50 border border-slate-200 rounded-lg p-5 sm:p-6 transition-all duration-300 hover:border-indigo-400
+             hover:bg-indigo-50">
+              <div className="relative z-10 flex items-center justify-between gap-4 sm:gap-5">
+                <h3 className="text-lg text-slate-800 group-hover:text-indigo-600 mb-1 transition-colors">{portal.title}</h3>
+                <ArrowRightIcon className="w-4 h-4 text-slate-400 group-hover:text-indigo-600 group-hover:translate-x-1 transition-all duration-300" />
+              </div>
+            </Link>
+          ))}
+        </div>
 
 
         {/* footer */}
+        <div>
+          <p> © {new Date().getFullYear()} WorkSphere. All Rights Reserved. </p>
+        </div>
 
 
       </div>
